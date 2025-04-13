7be1c547fd5087fabdea7d0c3e0bee7586e265bb
Tushar <142796801+tusharsno@users.noreply.github.com>
2025-04-13 22:45:00 +0600

    Add files via upload

---
+++ Trippi_Troppi.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i_64 = long long;
+using u_64 = unsigned long long;
+using u_32 = unsigned;
+using u_128 = unsigned __int128;
+
+
+void solve() {
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+
+    cout << s1[0] << s2[0] << s3[0] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
