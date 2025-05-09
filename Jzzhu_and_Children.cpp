4d92c925326d83fcad144939b699ceae84912cc1
Tushar <142796801+tusharsno@users.noreply.github.com>
2025-05-09 21:33:48 +0600

    Add files via upload

---
+++ Jzzhu_and_Children.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i_64 = long long;
+using u_64 = unsigned long long;
+using u_32 = unsigned;
+using u_128 = unsigned __int128;
+
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    while (n--) {
+        int a;
+        cin >> a;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //int t;
+    //cin >> t;
+
+    //while (t--) {
+        solve();
+    //}
+
+    return 0;
+}
