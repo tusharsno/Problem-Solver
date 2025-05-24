a585e6d7c669b51271a8d00d443864e94df5879f
Tushar <142796801+tusharsno@users.noreply.github.com>
2025-05-24 22:17:22 +0600

    Add files via upload

---
+++ Mahmoud_and_Ehab_and_the_even-odd_game.cpp
@@ -0,0 +1,29 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+using i_64 = long long;
+using u_64 = unsigned long long;
+using u_32 = unsigned;
+using u_128 = unsigned __int128;
+
+
+void solve() {
+    i_64 n;
+    cin >> n;
+
+    cout << (n % 2 == 0 ? "Mahmoud\n" : "Ehab\n");
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //int t;
+    //cin >> t;
+
+    //while (t--) {
+        solve();
+    //}
+
+    return 0;
+}
